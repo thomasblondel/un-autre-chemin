95f006e923573f19b2288d4dd8f2e855a535e3df
ThomasBlondel <contact@thomasblondel.fr>
2016-10-28 23:26:38 +0200

    Upload of the version 1

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+
+$(document).ready(function(){
+    $(".btn1").click(function(){
+        $("#myModal1").modal('show');
+    });
+
+    $(".btn2").click(function(){
+        $("#myModal2").modal('show');
+    });
+
+    $(".btn3").click(function(){
+        $("#myModal3").modal('show');
+    });
+
+    $(".btn4").click(function(){
+        $("#myModal4").modal('show');
+    });
+});
